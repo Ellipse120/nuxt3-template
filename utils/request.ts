e2d24dfce4c12847eecf135b00b432235dfb43e2
Lusai <ellipse120@gmail.com>
2022-05-09 17:12:29 +0800

    refactor: fix response type error

--- utils/request.ts
+++ utils/request.ts
@@ -4,10 +4,38 @@ interface ApiResponse {
   data: any
 }
 
-const base = 'http://localhost:3001/'
+const baseURL = 'http://localhost:3001/'
+const isDebug = localStorage.getItem('debug') === 'debug'
 
 const api = (url) => {
-  return useFetch(base + url)
+  return useFetch<ApiResponse>(url, {
+    baseURL,
+    // onRequest({ request, options }) {
+    //   console.log('[fetch request]', request, options)
+    // },
+    // @ts-ignore
+    onRequestError({ request, error }) {
+      // eslint-disable-next-line no-console
+      isDebug && console.log('[fetch request error]', request, error)
+    },
+    // @ts-ignore
+    onResponse({ request, response }) {
+      // eslint-disable-next-line no-console
+      isDebug && console.log('[fetch response]', request, response)
+    },
+    // @ts-ignore
+    onResponseError({ request, response }) {
+      // Log error
+      // eslint-disable-next-line no-console
+      isDebug &&
+        console.log(
+          '[fetch response error]',
+          request,
+          response.status,
+          response.body
+        )
+    },
+  })
 }
 
 export { api }
